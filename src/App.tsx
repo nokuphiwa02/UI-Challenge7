2d2e59da06a309a1fef10e8469402e1f34f899ef
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-07-07 15:45:02 +0200

    hero and Decoration

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 import { Navbar } from './components/Text/Navbar/Navbar'
 // import { Footer } from './components/Text/footer/footer'
 import {Hero} from './components/Text/Hero/Hero'
-
+import { Decoration } from './components/Text/Decoration/Decoration'
 
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
     <div id='scrollable'>
       <Navbar /> 
            <Hero />
+           <Decoration/>
       </div> 
       {/* <Footer /> */}
     </div>
---
+++ src/components/Text/Decoration/Decoration.tsx
@@ -0,0 +1,28 @@
+import { Text } from "../Text";
+import chair from "../../../assets/chair.jpg";
+import style from "./Decoration.module.css";
+
+export const Decoration = () => {
+  return (
+
+    <>
+    <div className= {style.container}>
+   
+
+      <div className={style["left"]}>
+        {/* <div className={style["Decoration-Text"]}> */}
+        <Text variant="h1">Modern Comfort Chair</Text>
+        <Text variant="h2">ROOM DECORATION</Text>
+        <Text variant="p">
+          Enjoy comfort and style everyday. Made with quality meterials for
+          long-lasting use. Perfect for your home or office . Simple, morden,
+          and elegent. A chair you'll love to use
+        </Text>
+      </div>
+      <div className={style.right}>
+      <img src={chair} alt="chair" />
+      </div>
+      </div>
+    </>
+  );
+};
